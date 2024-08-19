80f4f7d58646de90cf5b1708743acfb0b79495db
zohaib240 <zohaibjs123@gmail.com>
2024-08-18 23:14:44 -0700

    modify js files

--- dash.js
+++ dash.js
@@ -1,4 +1,4 @@
-import { collection, addDoc, getDocs , Timestamp , }
+import { collection, addDoc, getDocs , Timestamp ,deleteDoc ,doc ,updateDoc  }
  from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
   import { auth , db } from "./config.js";
 
@@ -10,32 +10,7 @@ const div=document.querySelector('#div')
 let arr =[];
 
 
-// data add on firestore ---->>
 
-form.addEventListener('submit',async (event) => {
-  div.innerHTML=''
-   div.innerHTML=''
-    event.preventDefault()
-   console.log(title.value)
-   console.log(discription.value)
-   console.log (auth)
-   try {
-    const docRef = await addDoc(collection(db, "posts"), {
-      Title: title.value,
-      Discription: discription.value,
-      uid : auth.currentUser.uid,
-      dateExample: Timestamp.fromDate(new Date())
-
-    });
-  
-    console.log("Document written with ID: ", docRef.id);
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
-    title.value='' 
-  discription.value=''
-  render()
-})
 
 // data get on firestore ---->>
 
@@ -44,38 +19,20 @@ async function getData() {
   arr.length=0
   const querySnapshot = await getDocs(collection(db, "posts"));
 querySnapshot.forEach((doc) => {
-    arr.push({...doc.data(),docId:doc.id})
+    arr.push({...doc.data(), id:doc.id})
 });
 console.log(arr);
 render()
 
-
-// delete edit option -----
-
-const del=document.querySelectorAll('#delete')
-const edit=document.querySelectorAll('#edit')
-
-
-del.forEach((btn ,index)=>{
-btn.addEventListener('click',()=>{
-console.log('del',arr[index]);
-arr.splice(index, 1)
-})
-})
-  
-
-edit.forEach((btn,index)=>{
-  btn.addEventListener('click',async ()=>{
-  console.log('edit',arr[index]);
- 
-  })
-})
 }
 getData()
+
+
 
 //  render function on screen--->>
 
 function render() {
+  div.innerHTML=''
   arr.map((item)=>{
     div.innerHTML +=`
   <div class="card" style="width: 30rem;">
@@ -83,34 +40,77 @@ function render() {
       <h5 class="card-title">${item.Title}</h5>
       <h6 class="card-subtitle mb-2 text-body-secondary">${item.Discription}</</h6>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <button type="button" id='edit' class=" mt-3 btn bg-primary btn-sm text-white">Edit</button>
-      <button type="button" id='delete'class=" mt-3 btn ml-5 btn-sm bg-danger text-white">delete</button>
+      <button type="button"id='delete'  class="delete  mt-3 btn ml-5 btn-sm bg-danger text-white">delete</button>
+      <button type="button" id='edit' class="edit  mt-3 btn bg-primary btn-sm text-white">Edit</button>
     </div>
   </div>
   ` 
-})
-
+  })
 // delete edit option -----
 
-const del=document.querySelectorAll('#delete')
+const del_manjan=document.querySelectorAll('#delete')
 const edit=document.querySelectorAll('#edit')
 
 
-del.forEach((btn)=>{
-btn.addEventListener('click',()=>{
-console.log('del');
-})
-  
+del_manjan.forEach((btn, index) => {
+  btn.addEventListener('click', async () => {
+    console.log('dfel',index);
+     await deleteDoc(doc(db, "posts", arr[index].id));
+      arr.splice(index, 1)
+      render()
+  })
 })
 
-edit.forEach((btn)=>{
-  btn.addEventListener('click',()=>{
-  console.log('edit');
+
+
+edit.forEach((btn,index)=>{
+  btn.addEventListener('click',async()=>{
+  console.log('edit',index);
+  let update = prompt('enter new title', arr[index].Title)
+  const washingtonRef = doc(db, "posts", arr[index].id);
+
+  await updateDoc(washingtonRef, {
+    Tilte:update 
+});
+if(update && update.trim() !== ''){
+    arr[index].Title = update
+}
+render()
   })
     
   })
 }
+render()
 
+
+// data add on firestore ---->>
+
+form.addEventListener('submit',async (event) => {
+  div.innerHTML=''
+   event.preventDefault()
+  console.log(title.value)
+  console.log(discription.value)
+  try {
+   const docRef = await addDoc(collection(db, "posts"), {
+     Title: title.value,
+     Discription: discription.value,
+     dateExample: Timestamp.fromDate(new Date())
+   });
+   arr.push({
+     Title: title.value,
+     Discription: discription.value,
+     dateExample: Timestamp.fromDate(new Date()),
+     id : docRef.id
+   })
+   render()
+   console.log("Document written with ID: ", docRef.id);
+ } catch (e) {
+   console.error("Error adding document: ", e);
+ }
+   title.value='' 
+ discription.value=''
+
+})
 
 
 
